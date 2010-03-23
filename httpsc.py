c4b413655f80537ad2c8c3197adc4f2beec67908
Benoît Allard <benoit.allard@gmx.de>
2010-03-23 21:58:46 +0100

    First version: establishcontext/listreaders/relesecontext

---
+++ httpsc.py
@@ -0,0 +1,68 @@
+import werkzeug
+
+from werkzeug.debug import DebuggedApplication
+
+import json
+
+from smartcard.scard import *
+
+# or should it be called WebSC ???
+class HTTPSC(object):
+    def __init__(self):
+        R = werkzeug.routing.Rule
+        self.url_map = werkzeug.routing.Map([
+            R('/', endpoint=self.welcome),
+            R('/EstablishContext/<scope>', endpoint=self.establishcontext),
+            R('/<context>/ListReaders/<readergroup>', endpoint=self.listreaders),
+            R('/<card>/Transmit/<apdu>', endpoint=self.transmit),
+            R('/<context>/ReleaseContext', endpoint=self.release),
+            ])
+
+    def welcome(self, request):
+        return werkzeug.Response("Welcome")
+
+    def establishcontext(self, request, scope):
+        hresult, hcontext = SCardEstablishContext(int(scope))
+        return werkzeug.Response(json.dumps({"hresult":hresult, "hcontext":hcontext, "HRformat": SCardGetErrorMessage(hresult)}))
+
+    def listreaders(self, request, context, readergroup):
+        hresult, readers = SCardListReaders( long(context), eval(readergroup) )
+        return werkzeug.Response(json.dumps({"hresult":hresult, "hcontext":context, "readers":readers, "HRformat": SCardGetErrorMessage(hresult)}))
+
+    def transmit(self, request, context, apdu):
+        pass
+
+    def release(self, request, context):
+        hresult = SCardReleaseContext(long(context))
+        return werkzeug.Response(json.dumps({"hresult":hresult, "HRformat": SCardGetErrorMessage(hresult)}))
+        
+
+    @werkzeug.responder
+    def application(self, environ, start):
+        """The main application loop."""
+
+        adapter = self.url_map.bind_to_environ(environ)
+        request = werkzeug.Request(environ)
+        endpoint, values = adapter.match()
+        return endpoint(request, **values)
+
+def application(env, start):
+    """Detect that we are being run as WSGI application."""
+
+    global application
+    httpsc = HTTPSC()
+    application = httpsc.application
+    return application(env, start)
+
+def debug(config=None, wiki=None):
+    """Start a standalone WSGI server."""
+
+    httpsc = HTTPSC()
+    app = DebuggedApplication(httpsc.application, evalex=True)
+    
+    host, port = ('0.0.0.0', 3333)
+
+    werkzeug.run_simple(host, port, app, use_reloader=True)
+
+if __name__ == "__main__":
+    debug()
