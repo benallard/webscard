9aec2df38c79a5fb52947d7c7651dcf3be74610e
Benoît Allard <benoit.allard@gmx.de>
2010-03-25 18:49:25 +0100

    client: pretty print the commands sent and received

--- luaclient/client.lua
+++ luaclient/client.lua
@@ -21,10 +21,14 @@ if #readers ~= 0 then
    hresult, hcard, protocol = scard.connect(hcontext, readers[1], 2 ,3)
    hresult, readername, state, protocol, atr = scard.status(hcard)
    print ("ATR: "..apdutostring(atr))
-   hresult, answer = scard.transmit(hcard, protocol, {0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00})
-   print ("Received: "..apdutostring(answer))
-   hresult, answer = scard.transmit(hcard, protocol, {0x80, 0xCA, 0x9F, 0x7F, 0x00})
-   print ("Received: "..apdutostring(answer))
+   apdu = {0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00}
+   print ("==> "..apdutostring(apdu))
+   hresult, answer = scard.transmit(hcard, protocol,apdu)
+   print ("<== "..apdutostring(answer))
+   apdu = {0x80, 0xCA, 0x9F, 0x7F, 0x00}
+   print ("==> "..apdutostring(apdu))
+   hresult, answer = scard.transmit(hcard, protocol, apdu)
+   print ("<== "..apdutostring(answer))
    hresult = scard.disconnect(hcard, 0)
 end
 hresult = scard.releasecontext(hcontext)
