f569d6e8b9e6f9444afcaf3eabbf9037ec19e4c3
Benoît Allard <benoit.allard@gmx.de>
2010-03-28 23:20:15 +0200

    refactor Happy Implementation

--- webscard/implementations/happy.py
+++ webscard/implementations/happy.py
@@ -1,16 +1,23 @@
 class Implementation:
-    
+
+    readername = "Happy Reader 0"
+    protocol = 2
+    ATR = [0x3B, 0x00]
+
     def SCardEstablishContext(self, dwScope):
         return 0, 0
 
     def SCardListReaders(self, hContext, ReaderGroup):
-        return 0, ["Happy Reader 0"]
+        return 0, [self.readername]
 
     def SCardReleaseContext(self, hContext):
         return 0
 
     def SCardConnect(self, hContext, zreader, dwShared, dwProtocol):
-        return 0, 0, 2
+        return 0, 0, self.protocol
+
+    def SCardStatus(self, hCard):
+        return 0, self.readername, 52, self.protocol, self.ATR
 
     def SCardTransmit(self, hContext, dwProtocol, apdu):
         return 0, [0x90, 0x00]
