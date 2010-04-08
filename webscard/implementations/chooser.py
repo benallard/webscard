4ca94b283a69dcde21d569082a166133f96bfb1b
Benoît Allard <benoit.allard@gmx.de>
2010-04-08 11:32:16 +0200

    We can also load implementations in files.
    Thanks to mercurial extension loading.
    The implementations pointed by %s.path and not only %s.module
    (those are mutual exclusive)

--- webscard/implementations/chooser.py
+++ webscard/implementations/chooser.py
@@ -1,3 +1,5 @@
+import imp, os
+
 from webscard.utils import application
 
 from webscard.implementations.pyscard import Implementation
@@ -16,17 +18,43 @@ class Chooser(object):
             if impl is not None:
                 self.pool.append(impl)
 
+    def loadpath(self, path, name):
+        """ taken from mercurial.extensions """
+        name = name.replace('.','_')
+        path = os.path.expanduser(os.path.expandvars(path))
+        if os.path.isdir(path):
+            # module/__init__.py style
+            d, f = os.path.split(path.rstrip(os.path.sep))
+            fd, fpath, desc = imp.find_module(f, [d])
+            return imp.load_module(name, fd, fpath, desc)
+        else:
+            return imp.load_source(name, path)
+
+    def getmodulefor(self, name):
+        cfg = application.config
+        path = cfg.getstring('%s.path' % name, None)
+        if path is not None:
+            # Generate a unique name to avoid clash
+            mod = self.loadpath(path, 'webscard.%s' % name)
+        else:
+            module = cfg.getstring('%s.module' % name, None)
+            if module is None:
+                if name == 'pyscard':
+                    module = 'smartcard.scard'
+                else:
+                    raise ValueError(
+                        '%s.module or %s.path config option missing' % name)
+            mod = __import__(module)
+            components = module.split('.')
+            for comp in components[1:]:
+                mod = getattr(mod, comp)
+        print "mod is %s" % mod
+        return mod
+
     def create(self, name):
         cfg = application.config
-        module = cfg.getstring('%s.module' % name, None)
-        if module is None:
-            if name == 'pyscard':
-                module = 'smartcard.scard'
-            else:
-                raise ValueError('%s.module config option missing' % name)
+        mod = self.getmodulefor(name)
         classname = cfg.getstring('%s.classname' % name, None)
-        mod = __import__(module, globals(), locals(), [''])
-        print "mod is %s" % mod
         if classname is None:
             impl = mod
         else:
