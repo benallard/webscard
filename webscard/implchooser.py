caa249de0d91a074df56c692f7d6174fc15e4fb6
Benoît Allard <benoit.allard@gmx.de>
2010-04-08 10:45:09 +0200

    Remove old unused since long time implchooser

--- webscard/implchooser.py
+++
@@ -1,6 +0,0 @@
-from webscard import pyscardimpl, happyimpl
-
-def chooseone():
-    """ If we have more than one choice, we should do something else """
-    return pyscardimpl.Implementation()
-        
