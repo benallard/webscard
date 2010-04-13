7b8d28bb0c930a9ede23e976ed0c091e5176164f
Benoît Allard <benoit.allard@gmx.de>
2010-04-13 16:51:42 +0200

    We now have a webserver inside the qt systray icon.

--- manage.py
+++ manage.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 from werkzeug import script
 
+from webscard.config import Config
+
 CONFIG_FILE = 'webscard.cfg'
+
+config = Config(CONFIG_FILE)
 
 def make_app():
     from webscard.application import WebSCard
-    from webscard.config import Config
-    return WebSCard(Config(CONFIG_FILE))
+    return WebSCard(config)
 
 def make_shell():
     application = make_app()
     return locals()
 
 action_runserver = script.make_runserver(
-    make_app, hostname='0.0.0.0', port=3333, use_reloader=True, extra_files=CONFIG_FILE, use_debugger=True)
+    make_app, hostname=config.getstring('web.host', '0.0.0.0'), 
+    port=config.getinteger('web.port', 3333), 
+    use_reloader=True, extra_files=CONFIG_FILE, use_debugger=True)
 action_initdb = lambda: make_app().init_database()
 action_shell = script.make_shell(make_shell)
 
--- webscard/icon/qt.py
+++ webscard/icon/qt.py
@@ -1,10 +1,29 @@
 import sys
 
 from PyQt4.QtGui import (QApplication, QSystemTrayIcon, QAction, QMenu, QIcon, QPixmap)
-from PyQt4.QtCore import (Qt, QString)
+from PyQt4.QtCore import (Qt, QString, QThread)
+
+from webscard.application import WebSCard
+from webscard.config import Config
+
+import werkzeug
+from werkzeug import run_simple
+
+class WebServerThread(QThread):
+    def __init__(self, config):
+        QThread.__init__(self)
+        self.config = config
+        wscard = WebSCard(config)
+        self.app = werkzeug._easteregg(wscard)
+
+    def run(self):
+        run_simple(self.config.getstring('web.host', '0.0.0.0'),
+                   self.config.getinteger('web.port', 3333),
+                   self.app)
+        
 
 class WebSCardTrayIcon(QSystemTrayIcon):
-    def __init__(self):
+    def __init__(self, config):
         QSystemTrayIcon.__init__(self)
         self.setIcon(QIcon(QPixmap('Z:\\Spider.web.logo.png')))
         self.menu = QMenu(QString("WebScard Menu"))
@@ -18,7 +37,10 @@ class WebSCardTrayIcon(QSystemTrayIcon):
 
         self.setToolTip(QString(u'Click this icon to interact with'
                                 u' WebSCard.'))
-   
+
+        self.webserverthread = WebServerThread(config)
+        self.webserverthread.start()
+
         self.show()
 
     def on_quit(self):
@@ -28,11 +50,16 @@ class WebSCardTrayIcon(QSystemTrayIcon):
         global app
         app.quit()
 
-if __name__ == "__main__":
+def make_app():
+    config = Config('Z:\\WSCenv\\webscard\\webscard.cfg')
     try:
+        global app
         app = QApplication(sys.argv)
         QApplication.setQuitOnLastWindowClosed(False)
-        status_icon = WebSCardTrayIcon()
+        status_icon = WebSCardTrayIcon(config)
         app.exec_()
     except KeyboardInterrupt:
-        app.quit()
+        pass
+    
+if __name__ == "__main__":
+    make_app()
