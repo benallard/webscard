71e115f3d0495ae9bca63d6276b08db9268630d1
Benoît Allard <benoit.allard@gmx.de>
2010-12-09 23:03:42 +0100

    Restore ** when passing arg, don't remove what you don't understand

--- webscard/utils.py
+++ webscard/utils.py
@@ -29,7 +29,7 @@ url_map = Map(redirect_defaults=False)
 def expose(rule, **kw):
     def decorate(f):
         kw['endpoint'] = f.__name__
-        url_map.add(Rule(rule, kw))
+        url_map.add(Rule(rule, **kw))
         return f
     return decorate
 
