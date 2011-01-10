1aeb8c7269c57a5487b5ea7e979d92b8df376eca
Benoît Allard <benoit.allard@gmx.de>
2011-01-10 22:42:44 +0100

    Add a static middleware

--- webscard/utils.py
+++ webscard/utils.py
@@ -18,7 +18,7 @@ metadata = MetaData()
 dbsession = scoped_session(sessionmaker(autocommit=True), 
                            local_manager.get_ident)
 
-url_map = Map(redirect_defaults=False)
+url_map = Map([Rule('/static/<file>', endpoint='static', build_only=True)],redirect_defaults=False)
 def expose(rule, **kw):
     def decorate(f):
         kw['endpoint'] = f.__name__
@@ -38,6 +38,11 @@ def get_main_dir():
     if main_is_frozen():
         return os.path.dirname(sys.executable)
     return os.path.dirname(sys.argv[0])
+
+def get_static_dir():
+    if main_is_frozen():
+        return os.path.join(get_main_dir(), 'static')
+    return os.path.join(os.path.dirname(__file__), 'static')
 
 def unsigned_long(s):
     return int(s) % (2**32-1)
